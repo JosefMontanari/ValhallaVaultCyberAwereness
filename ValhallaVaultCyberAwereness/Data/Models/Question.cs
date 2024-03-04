30742bbd71e603cc62849ea70df8bca365049fd4
JosefMontanari <josef.montanari94@gmail.com>
2024-03-04 13:27:34 +0100

    remade dbsets

--- ValhallaVaultCyberAwereness/Data/Models/Question.cs
+++ ValhallaVaultCyberAwereness/Data/Models/Question.cs
@@ -5,17 +5,15 @@ namespace ValhallaVaultCyberAwereness.Data.Models;
 public class Question
 {
 
-    [Key]
-    public int QuestionId { get; set; }
+	[Key]
+	public int QuestionId { get; set; }
 
-    public string Questions { get; set; }
+	public string Questions { get; set; }
 
-    public List<Answer> Answers { get; set; } = new List<Answer>();// flera answers
+	public List<string> PossibleAnswers { get; set; }
 
-    public List<string> PossibleAnswers { get; set; }
+	public string CorrectAnswer { get; set; }
+	public Segment Segment { get; set; } // har en segment
 
-    public string CorrectAnswer { get; set; }
-    public Segment Segment { get; set; } // har en segment
-
-    public int SegmentId { get; set; }
+	public int SegmentId { get; set; }
 }
