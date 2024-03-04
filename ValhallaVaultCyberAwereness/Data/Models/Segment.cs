d7cdbace57d53beac7d62327246dac15c2dff6f6
hdfredl <dat.leef@gmail.com>
2024-03-04 11:27:45 +0100

    Skapat klasser och props

--- ValhallaVaultCyberAwereness/Data/Models/Segment.cs
+++ ValhallaVaultCyberAwereness/Data/Models/Segment.cs
@@ -1,6 +1,17 @@
-﻿namespace ValhallaVaultCyberAwereness.Data.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ValhallaVaultCyberAwereness.Data.Models;
+
+public class Segment
 {
-	public class Segment
-	{
-	}
+    [Key]
+    public int SegmentId { get; set; }
+    public string SegmentTitle { get; set; }
+
+    public Category Category { get; set; } // Har en category
+
+    public int CategoryId { get; set; }
+
+    public List<Question> Question { get; set; } = new List<Question>(); // har flera questions
+
 }
