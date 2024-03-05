00b29bac1bc56b9b93738f27bef161e2f07b553f
JosefMontanari <josef.montanari94@gmail.com>
2024-03-05 20:44:47 +0100

    adminDashboard

--- ValhallaVaultCyberAwereness/Service/CategoryRepo.cs
+++ ValhallaVaultCyberAwereness/Service/CategoryRepo.cs
@@ -5,61 +5,61 @@ using ValhallaVaultCyberAwereness.Data.Models;
 
 namespace ValhallaVaultCyberAwereness.Service
 {
-    public class CategoryRepo(ApplicationDbContext context)
-    {
+	public class CategoryRepo(ApplicationDbContext context)
+	{
 
-        private readonly ApplicationDbContext context;
+		private readonly ApplicationDbContext context;
 
-        private readonly ApplicationDbContext _context;
+		private readonly ApplicationDbContext _context;
 
 
 
-        public List<Category> categories { get; set; } = new List<Category>();
-        public async Task<List<Category>> GetAllCategoriesAsync()
-        {
-            categories = await context.Categories
-                .Include(x => x.Segments)
-                .ToListAsync();
+		public List<Category> categories { get; set; } = new List<Category>();
+		public async Task<List<Category>> GetAllCategoriesAsync()
+		{
+			categories = await context.Categories
+				.Include(x => x.Segments)
+				.ToListAsync();
 
-            return await context.Categories.
-                Include(x => x.Segments).ToListAsync();
+			return await context.Categories.
+				Include(x => x.Segments).ToListAsync();
 
-        }
-        public async Task<Category?> GetCategoryByIdAsync(int id)
-        {
-            return await context.Categories.FirstOrDefaultAsync(c => c.CategoryId == id);
-        }
+		}
+		public async Task<Category?> GetCategoryByIdAsync(int id)
+		{
+			return await context.Categories.FirstOrDefaultAsync(c => c.CategoryId == id);
+		}
 
-        public async Task AddCategoryAsync(Category categoryToAdd)
-        {
-            await context.Categories.AddAsync(categoryToAdd);
-            await context.SaveChangesAsync();
-        }
+		public async Task AddCategoryAsync(Category categoryToAdd)
+		{
+			await context.Categories.AddAsync(categoryToAdd);
+			await context.SaveChangesAsync();
+		}
 
-        public async Task UpdateCategoryAsync(Category updatedCategory)
-        {
-            Category? categoryToUpdate = await GetCategoryByIdAsync(updatedCategory.CategoryId);
+		public async Task UpdateCategoryAsync(Category updatedCategory)
+		{
+			Category? categoryToUpdate = await GetCategoryByIdAsync(updatedCategory.CategoryId);
 
-            if (categoryToUpdate == null)
-            {
-                categoryToUpdate.Categories = updatedCategory.Categories;
+			if (categoryToUpdate == null)
+			{
+				categoryToUpdate.Categories = updatedCategory.Categories;
 
-                await context.SaveChangesAsync();
-            }
-        }
+				await context.SaveChangesAsync();
+			}
+		}
 
-        public async Task DeleteCategoryAsync(Category categoryToDelete)
-        {
-            try
-            {
-                context.Categories.Remove(categoryToDelete);
-                await context.SaveChangesAsync();
-            }
-            catch
-            {
+		public async Task DeleteCategoryAsync(Category categoryToDelete)
+		{
+			try
+			{
+				context.Categories.Remove(categoryToDelete);
+				await context.SaveChangesAsync();
+			}
+			catch
+			{
 
-            }
-        }
+			}
+		}
 
-    }
+	}
 }
