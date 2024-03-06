59d1d56326368b8207743bfa4b5b321c87f9e0fe
JosefMontanari <josef.montanari94@gmail.com>
2024-03-06 13:28:53 +0100

    edit page parameters and repos

--- ValhallaVaultCyberAwereness/Service/SegmentRepo.cs
+++ ValhallaVaultCyberAwereness/Service/SegmentRepo.cs
@@ -4,27 +4,28 @@ using ValhallaVaultCyberAwereness.Data.Models;
 
 namespace ValhallaVaultCyberAwereness.Service
 {
-    public class SegmentRepo(ApplicationDbContext context)
-    {
+	public class SegmentRepo(ApplicationDbContext context)
+	{
 
-        public List<Segment> segments { get; set; } = new List<Segment>();
+		public List<Segment> segments { get; set; } = new List<Segment>();
 
-        public async Task<List<Segment>> GetAllSegmentAsync()
-        {
-            segments = await context.Segments
-                .Include(q => q.Question)
-                .ToListAsync();
+		public async Task<List<Segment>> GetAllSegmentAsync()
+		{
+			segments = await context.Segments
+				.Include(q => q.Question)
+				.ToListAsync();
 
-            return await context.Segments
-                .Include(q => q.Question)
-                .ToListAsync();
-        }
-        public async Task<Segment?> GetSegmentByIdAsync(int id)
-        {
-            return await context.Segments.FirstOrDefaultAsync(s => s.SegmentId == id);
-        }
-
-			}
+			return await context.Segments
+				.Include(q => q.Question)
+				.ToListAsync();
 		}
+		public async Task<Segment?> GetSegmentByIdAsync(int id)
+		{
+			return await context.Segments.FirstOrDefaultAsync(s => s.SegmentId == id);
+		}
+
 	}
 }
+
+
+
