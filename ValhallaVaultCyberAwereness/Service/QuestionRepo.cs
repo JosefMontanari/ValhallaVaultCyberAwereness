7f828047f9b3b21cc3012ed2ae6cc4914558847e
JosefMontanari <josef.montanari94@gmail.com>
2024-03-07 11:04:18 +0100

    add editforms for all tables

--- ValhallaVaultCyberAwereness/Service/QuestionRepo.cs
+++ ValhallaVaultCyberAwereness/Service/QuestionRepo.cs
@@ -4,54 +4,60 @@ using ValhallaVaultCyberAwereness.Data.Models;
 
 namespace ValhallaVaultCyberAwereness.Service
 {
-    public class QuestionRepo(ApplicationDbContext context)
-    {
-        public List<Question> questions { get; set; } = new List<Question>();
+	public class QuestionRepo(ApplicationDbContext context)
+	{
+		public List<Question> questions { get; set; } = new List<Question>();
 
-        public async Task<List<Question>> GetAllQuestionAsync()
-        {
-            questions = await context.Questions
-                .Include(x => x.Segment)
-                .ThenInclude(c => c.Category)
-                .ToListAsync();
+		public async Task<List<Question>> GetAllQuestionAsync()
+		{
+			questions = await context.Questions
+				.Include(x => x.Segment)
+				.ThenInclude(c => c.Category)
+				.ToListAsync();
 
-            return await context.Questions.
-                Include(x => x.Segment).
-                ThenInclude(c => c.Category).ToListAsync();
-        }
+			return await context.Questions.
+				Include(x => x.Segment).
+				ThenInclude(c => c.Category).ToListAsync();
+		}
 
-        public async Task<Question?> GetQuestionByIdAsync(int id)
-        {
-            return await context.Questions.FirstOrDefaultAsync(q => q.QuestionId == id);
-        }
-        public async Task AddQuestionAsync(Question questionToAdd)
-        {
-            await context.Questions.AddAsync(questionToAdd);
-            await context.SaveChangesAsync();
-        }
+		public async Task<Question?> GetQuestionByIdAsync(int id)
+		{
+			return await context.Questions.FirstOrDefaultAsync(q => q.QuestionId == id);
+		}
+		public async Task AddQuestionAsync(Question questionToAdd)
+		{
+			await context.Questions.AddAsync(questionToAdd);
+			await context.SaveChangesAsync();
+		}
 
-        public async Task UpdateQuestionAsync(Question updatedQuestion)
-        {
-            Question? QuestionUpdate = await GetQuestionByIdAsync(updatedQuestion.QuestionId);
+		public async Task UpdateQuestionAsync(Question updatedQuestion)
+		{
+			Question? QuestionUpdate = await GetQuestionByIdAsync(updatedQuestion.QuestionId);
 
-            if (QuestionUpdate != null)
-            {
-                QuestionUpdate.Questions = updatedQuestion.Questions;
+			if (QuestionUpdate != null)
+			{
+				QuestionUpdate.Questions = updatedQuestion.Questions;
+				QuestionUpdate.Title = updatedQuestion.Title;
+				QuestionUpdate.PossibleAnswers = updatedQuestion.PossibleAnswers;
+				QuestionUpdate.CorrectAnswer = updatedQuestion.CorrectAnswer;
+				QuestionUpdate.Segment = updatedQuestion.Segment;
 
-                await context.SaveChangesAsync();
-            }
-        }
-        public async Task DeleteQuestionAsync(Question questionToDelete)
-        {
-            try
-            {
-                context.Questions.Remove(questionToDelete);
-                await context.SaveChangesAsync();
-            }
-            catch
-            {
 
-            }
-        }
-    }
+
+				await context.SaveChangesAsync();
+			}
+		}
+		public async Task DeleteQuestionAsync(Question questionToDelete)
+		{
+			try
+			{
+				context.Questions.Remove(questionToDelete);
+				await context.SaveChangesAsync();
+			}
+			catch
+			{
+
+			}
+		}
+	}
 }
--- ValhallaVaultCyberAwereness/Service/SegmentRepo.cs
+++ ValhallaVaultCyberAwereness/Service/SegmentRepo.cs
@@ -4,60 +4,61 @@ using ValhallaVaultCyberAwereness.Data.Models;
 
 namespace ValhallaVaultCyberAwereness.Service
 {
-    public class SegmentRepo(ApplicationDbContext context)
-    {
+	public class SegmentRepo(ApplicationDbContext context)
+	{
 
-        public List<Segment> segments { get; set; } = new List<Segment>();
+		public List<Segment> segments { get; set; } = new List<Segment>();
 
-        public async Task<List<Segment>> GetAllSegmentAsync()
-        {
-            segments = await context.Segments
-                .Include(q => q.Question)
-                .ToListAsync();
+		public async Task<List<Segment>> GetAllSegmentAsync()
+		{
+			segments = await context.Segments
+				.Include(q => q.Question)
+				.ToListAsync();
 
-            return await context.Segments
-                .Include(q => q.Question)
-                .ToListAsync();
-        }
-        public async Task<Segment?> GetSegmentByIdAsync(int id)
-        {
-            return await context.Segments.FirstOrDefaultAsync(s => s.SegmentId == id);
-        }
-        public async Task<List<Segment>> GetSegmentsByCategoryIdAsync(int categoryId)
-        {
-            return await context.Segments
-                .Include(q => q.Question)
-                .Where(s => s.CategoryId == categoryId)
-                .ToListAsync();
-        }
-        public async Task AddSegmentAsync(Segment segmentToAdd)
-        {
-            await context.Segments.AddAsync(segmentToAdd);
-            await context.SaveChangesAsync();
-        }
-        public async Task UpdateSegmentAsync(Segment updatedSegment)
-        {
-            Segment? SegmentUpdate = await GetSegmentByIdAsync(updatedSegment.SegmentId);
+			return await context.Segments
+				.Include(q => q.Question)
+				.ToListAsync();
+		}
+		public async Task<Segment?> GetSegmentByIdAsync(int id)
+		{
+			return await context.Segments.FirstOrDefaultAsync(s => s.SegmentId == id);
+		}
+		public async Task<List<Segment>> GetSegmentsByCategoryIdAsync(int categoryId)
+		{
+			return await context.Segments
+				.Include(q => q.Question)
+				.Where(s => s.CategoryId == categoryId)
+				.ToListAsync();
+		}
+		public async Task AddSegmentAsync(Segment segmentToAdd)
+		{
+			await context.Segments.AddAsync(segmentToAdd);
+			await context.SaveChangesAsync();
+		}
+		public async Task UpdateSegmentAsync(Segment updatedSegment)
+		{
+			Segment? SegmentUpdate = await GetSegmentByIdAsync(updatedSegment.SegmentId);
 
-            if (SegmentUpdate != null)
-            {
+			if (SegmentUpdate != null)
+			{
 
-                SegmentUpdate.SegmentTitle = updatedSegment.SegmentTitle;
+				SegmentUpdate.SegmentTitle = updatedSegment.SegmentTitle;
+				SegmentUpdate.Category = updatedSegment.Category;
 
-                await context.SaveChangesAsync();
-            }
-        }
-        public async Task DeleteSegment(Segment segmentToDelete)
-        {
-            try
-            {
-                context.Segments.Remove(segmentToDelete);
-                await context.SaveChangesAsync();
-            }
-            catch
-            {
+				await context.SaveChangesAsync();
+			}
+		}
+		public async Task DeleteSegment(Segment segmentToDelete)
+		{
+			try
+			{
+				context.Segments.Remove(segmentToDelete);
+				await context.SaveChangesAsync();
+			}
+			catch
+			{
 
-            }
-        }
-    }
+			}
+		}
+	}
 }
