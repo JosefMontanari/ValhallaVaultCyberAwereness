57ad6590b3e5dd0839a05723c1d01a227aaf5bc5
JosefMontanari <josef.montanari94@gmail.com>
2024-03-07 13:39:41 +0100

    Fix delete behaviour

---
+++ ValhallaVaultCyberAwereness/Data/Migrations/20240307123341_deletebehaviourtest.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ValhallaVaultCyberAwereness.Migrations
+{
+    /// <inheritdoc />
+    public partial class deletebehaviourtest : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Segments_Categories_CategoryId",
+                table: "Segments");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CategoryId",
+                table: "Segments",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Segments_Categories_CategoryId",
+                table: "Segments",
+                column: "CategoryId",
+                principalTable: "Categories",
+                principalColumn: "CategoryId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Segments_Categories_CategoryId",
+                table: "Segments");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CategoryId",
+                table: "Segments",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Segments_Categories_CategoryId",
+                table: "Segments",
+                column: "CategoryId",
+                principalTable: "Categories",
+                principalColumn: "CategoryId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
--- ValhallaVaultCyberAwereness/Data/Models/Category.cs
+++ ValhallaVaultCyberAwereness/Data/Models/Category.cs
@@ -4,12 +4,12 @@ namespace ValhallaVaultCyberAwereness.Data.Models;
 
 public class Category
 {
-    [Key]
-    public int CategoryId { get; set; }
+	[Key]
+	public int CategoryId { get; set; }
 
-    public string Categories { get; set; }
+	public string Categories { get; set; }
 
-    public List<Segment> Segments { get; set; } = new List<Segment>(); // har flera segment
+	public List<Segment>? Segments { get; set; } = new List<Segment>(); // har flera segment
 
-    //public int SegmentId { get; set; } // F-key 
+	//public int SegmentId { get; set; } // F-key 
 }
