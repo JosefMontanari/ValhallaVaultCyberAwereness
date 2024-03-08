b715fd8307eb7f54cce57491ce34775c3605027f
hdfredl <dat.leef@gmail.com>
2024-03-08 09:58:30 +0100

    gjort check för question page så vi inte adderar dubbelt

--- ValhallaVaultCyberAwereness/Data/Models/AnswerUser.cs
+++ ValhallaVaultCyberAwereness/Data/Models/AnswerUser.cs
@@ -2,19 +2,20 @@
 
 namespace ValhallaVaultCyberAwereness.Data.Models
 {
-    public class AnswerUser
-    {
-        [Parameter]
-        public int id { get; set; }
-        // Lagt till denna för att store:a guid.
-        //public string UserId { get; set; } <- Kan behövas för att räkna ut avklarade %. UserId är en Guid/string i Db och inte Int.
-        public ApplicationUser? User { get; set; }
-        public int QuestionId { get; set; }
-        public Question? Question { get; set; }
-        public bool IsAnswerCorrect { get; set; }
+	public class AnswerUser
+	{
+		[Parameter]
+		public int id { get; set; }
+		// Lagt till denna för att store:a guid.
+		//public string UserId { get; set; } <- Kan behövas för att räkna ut avklarade %. UserId är en Guid/string i Db och inte Int.
+		public ApplicationUser? User { get; set; }
+		public int QuestionId { get; set; }
+		public Question? Question { get; set; }
+		public bool IsAnswerCorrect { get; set; }
 
-        // Behöver spara en users svar i denna klass
-        public string? UserAnswer { get; set; }
+		// Behöver spara en users svar i denna klass
 
-    }
+		public string? UserAnswer { get; set; }
+
+	}
 }
