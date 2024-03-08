da5bed65e0edd54cbaae9378289fdf69ebac31d6
hdfredl <dat.leef@gmail.com>
2024-03-08 22:52:57 +0100

    lagt till middleware, logger

---
+++ ValhallaVaultCyberAwereness/FredriksMiddlewareHandler/MWFServices.cs
@@ -0,0 +1,23 @@
+﻿namespace ValhallaVaultCyberAwereness.FredriksMiddlewareHandler;
+
+public class MWFServices
+{
+    private readonly RequestDelegate _next;
+    private readonly ILogger<MWFServices> _logger;
+    public MWFServices(RequestDelegate next, ILogger<MWFServices> logger)
+    {
+        _next = next;
+        _logger = logger;
+    }
+
+    public async Task Invoke(HttpContext context)
+    {
+        // Logga msg om man är i någon av dessa sidor
+        if (context.Request.Path.StartsWithSegments("/categorypage") || context.Request.Path.StartsWithSegments("/segmentpage") || context.Request.Path.StartsWithSegments("/subcategory") || context.Request.Path.StartsWithSegments("/questionpage"))
+        {
+            _logger.LogInformation("REQUEST received at: " + DateTime.Now + "(managed by THE LEGENDARY SPACE KNIGHTS OF THE GREAT SCANDINAVIA)");
+        }
+        // hanterar nästa request
+        await _next(context);
+    }
+}
