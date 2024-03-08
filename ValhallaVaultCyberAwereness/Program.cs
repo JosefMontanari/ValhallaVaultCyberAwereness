b7c289c257424422a9fa6b5fc2f18a02de9716e3
Tim Nordin <74505667+Sidus-96@users.noreply.github.com>
2024-03-08 19:02:28 +0100

    Implemented a catch for 404 error status code, redirects to my 404 page

--- ValhallaVaultCyberAwereness/Program.cs
+++ ValhallaVaultCyberAwereness/Program.cs
@@ -11,7 +11,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
-	.AddInteractiveServerComponents();
+    .AddInteractiveServerComponents();
 
 builder.Services.AddCascadingAuthenticationState();
 builder.Services.AddScoped<IdentityUserAccessor>();
@@ -27,102 +27,102 @@ builder.Services.AddScoped<AnswerRepo>();
 builder.Services.AddBlazoredLocalStorage();
 
 builder.Services.AddAuthentication(options =>
-	{
-		options.DefaultScheme = IdentityConstants.ApplicationScheme;
-		options.DefaultSignInScheme = IdentityConstants.ExternalScheme;
-	})
-	.AddIdentityCookies();
+    {
+        options.DefaultScheme = IdentityConstants.ApplicationScheme;
+        options.DefaultSignInScheme = IdentityConstants.ExternalScheme;
+    })
+    .AddIdentityCookies();
 
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-	options.UseSqlServer(connectionString));
+    options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddIdentityCore<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
-	.AddRoles<IdentityRole>()
-	.AddEntityFrameworkStores<ApplicationDbContext>()
-	// Lägger till signinmanager och usermanager
-	.AddSignInManager<SignInManager<ApplicationUser>>()
-	.AddUserManager<UserManager<ApplicationUser>>()
-	.AddDefaultTokenProviders();
+    .AddRoles<IdentityRole>()
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    // Lägger till signinmanager och usermanager
+    .AddSignInManager<SignInManager<ApplicationUser>>()
+    .AddUserManager<UserManager<ApplicationUser>>()
+    .AddDefaultTokenProviders();
 
 builder.Services.AddSingleton<IEmailSender<ApplicationUser>, IdentityNoOpEmailSender>();
 using (ServiceProvider sp = builder.Services.BuildServiceProvider())
 {
-	var context = sp.GetRequiredService<ApplicationDbContext>();
-	var signInManager = sp.GetRequiredService<SignInManager<ApplicationUser>>();
-	var roleManager = sp.GetRequiredService<RoleManager<IdentityRole>>();
+    var context = sp.GetRequiredService<ApplicationDbContext>();
+    var signInManager = sp.GetRequiredService<SignInManager<ApplicationUser>>();
+    var roleManager = sp.GetRequiredService<RoleManager<IdentityRole>>();
 
-	// Kolla om det finns en databas
-	context.Database.Migrate();
+    // Kolla om det finns en databas
+    context.Database.Migrate();
 
-	ApplicationUser newAdmin = new()
-	{
-		UserName = "Admin",
-		Email = "adminUser@mail.com",
-		EmailConfirmed = true
+    ApplicationUser newAdmin = new()
+    {
+        UserName = "Admin",
+        Email = "adminUser@mail.com",
+        EmailConfirmed = true
 
-	};
+    };
 
-	ApplicationUser newUser = new()
-	{
-		UserName = "User",
-		Email = "User@mail.com",
-		EmailConfirmed = true
+    ApplicationUser newUser = new()
+    {
+        UserName = "User",
+        Email = "User@mail.com",
+        EmailConfirmed = true
 
-	};
+    };
 
-	var admin = signInManager.UserManager.FindByEmailAsync(newAdmin.Email)
-		// Kör Metoden synkront!Viktigt! 
-		.GetAwaiter().GetResult();
-	if (admin == null)
-	{
-		signInManager.UserManager.CreateAsync(newAdmin, "Password1234!")
-			.GetAwaiter().GetResult();
-		admin = newAdmin;
+    var admin = signInManager.UserManager.FindByEmailAsync(newAdmin.Email)
+        // Kör Metoden synkront!Viktigt! 
+        .GetAwaiter().GetResult();
+    if (admin == null)
+    {
+        signInManager.UserManager.CreateAsync(newAdmin, "Password1234!")
+            .GetAwaiter().GetResult();
+        admin = newAdmin;
 
-		// Kolla om adminrollen existerar
-		bool adminRoleExists = roleManager.RoleExistsAsync("Admin")
-			// Kör metoden Synkront! Viktigt!
-			.GetAwaiter().GetResult();
-		if (!adminRoleExists)
-		{
-			// Skapa adminrollen
-			IdentityRole adminRole = new()
-			{
-				Name = "Admin",
-			};
+        // Kolla om adminrollen existerar
+        bool adminRoleExists = roleManager.RoleExistsAsync("Admin")
+            // Kör metoden Synkront! Viktigt!
+            .GetAwaiter().GetResult();
+        if (!adminRoleExists)
+        {
+            // Skapa adminrollen
+            IdentityRole adminRole = new()
+            {
+                Name = "Admin",
+            };
 
-			roleManager.CreateAsync(adminRole)
-			// Kör metoden Synkront! Viktigt!
-			.GetAwaiter().GetResult();
-		}
-		// Tilldela adminrollen till den nya användaren
-		signInManager.UserManager.AddToRoleAsync(admin, "Admin")
-	   // Kör metoden Synkront! Viktigt!
-	   .GetAwaiter().GetResult();
-	}
-
-
+            roleManager.CreateAsync(adminRole)
+            // Kör metoden Synkront! Viktigt!
+            .GetAwaiter().GetResult();
+        }
+        // Tilldela adminrollen till den nya användaren
+        signInManager.UserManager.AddToRoleAsync(admin, "Admin")
+       // Kör metoden Synkront! Viktigt!
+       .GetAwaiter().GetResult();
+    }
 
 
-	var user = signInManager.UserManager.FindByEmailAsync(newUser.Email)
+
+
+    var user = signInManager.UserManager.FindByEmailAsync(newUser.Email)
 // Kör Metoden synkront!Viktigt! 
 .GetAwaiter().GetResult();
-	if (user == null)
-	{
-		// Skapa ny user
-		signInManager.UserManager.CreateAsync(newUser, "Password1234!")
-		// Kör metoden Synkront! Viktigt!
-		.GetAwaiter().GetResult();
-	}
-	if (admin == null)
-	{
-		// Skapa ny admin
-		signInManager.UserManager.CreateAsync(newAdmin, "Password1234!")
-		// Kör metoden Synkront! Viktigt!
-		.GetAwaiter().GetResult();
-	}
+    if (user == null)
+    {
+        // Skapa ny user
+        signInManager.UserManager.CreateAsync(newUser, "Password1234!")
+        // Kör metoden Synkront! Viktigt!
+        .GetAwaiter().GetResult();
+    }
+    if (admin == null)
+    {
+        // Skapa ny admin
+        signInManager.UserManager.CreateAsync(newAdmin, "Password1234!")
+        // Kör metoden Synkront! Viktigt!
+        .GetAwaiter().GetResult();
+    }
 
 }
 
@@ -130,21 +130,21 @@ using (ServiceProvider sp = builder.Services.BuildServiceProvider())
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddControllersWithViews()
-	.AddJsonOptions(options =>
-	{
-		options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.IgnoreCycles;
-	});
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.IgnoreCycles;
+    });
 
 builder.Services.AddSwaggerGen();
 builder.Services.AddCors(options =>
 {
-	options.AddPolicy("AllowAll",
-			policy =>
-			{
-				policy.AllowAnyOrigin();
-				policy.AllowAnyHeader();
-				policy.AllowAnyMethod();
-			});
+    options.AddPolicy("AllowAll",
+            policy =>
+            {
+                policy.AllowAnyOrigin();
+                policy.AllowAnyHeader();
+                policy.AllowAnyMethod();
+            });
 });
 
 
@@ -156,13 +156,13 @@ app.UseSwaggerUI();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-	app.UseMigrationsEndPoint();
+    app.UseMigrationsEndPoint();
 }
 else
 {
-	app.UseExceptionHandler("/Error", createScopeForErrors: true);
-	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-	app.UseHsts();
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
 }
 app.UseCors("AllowAll");
 app.UseHttpsRedirection();
@@ -171,7 +171,7 @@ app.UseStaticFiles();
 app.UseAntiforgery();
 
 app.MapRazorComponents<App>()
-	.AddInteractiveServerRenderMode();
+    .AddInteractiveServerRenderMode();
 
 // Add additional endpoints required by the Identity /Account Razor components.
 app.MapAdditionalIdentityEndpoints();
@@ -179,5 +179,5 @@ app.MapAdditionalIdentityEndpoints();
 //för api
 app.MapControllers();
 //för api
-
+app.UseStatusCodePagesWithRedirects("/NotFoundStatus-{0}");
 app.Run();
