73ffac480714a8aefa1d59b201195bfa4f42ba4f
Tim Nordin <74505667+Sidus-96@users.noreply.github.com>
2024-03-11 00:49:39 +0100

    större ändring i questionPage

--- ValhallaVaultCyberAwereness/Service/QuestionRepo.cs
+++ ValhallaVaultCyberAwereness/Service/QuestionRepo.cs
@@ -5,59 +5,72 @@ using ValhallaVaultCyberAwereness.Data.Models;
 namespace ValhallaVaultCyberAwereness.Service
 {
 
-    public class QuestionRepo(ApplicationDbContext context)
-    {
-        public List<Question> questions { get; set; } = new List<Question>();
+	public class QuestionRepo(ApplicationDbContext context)
+	{
+		public List<Question> questions { get; set; } = new List<Question>();
 
-        public async Task<List<Question>> GetAllQuestionAsync()
-        {
-            questions = await context.Questions
-                .Include(x => x.Segment)
-                .ThenInclude(c => c.Category)
-                .ToListAsync();
+		public async Task<List<Question>> GetAllQuestionAsync()
+		{
+			questions = await context.Questions
+				.Include(x => x.Segment)
+				.ThenInclude(c => c.Category)
+				.ToListAsync();
 
-            return await context.Questions.
-                Include(x => x.Segment).
-                ThenInclude(c => c.Category).ToListAsync();
-        }
+			return await context.Questions.
+				Include(x => x.Segment).
+				ThenInclude(c => c.Category).ToListAsync();
+		}
 
-        public async Task<Question?> GetQuestionByIdAsync(int id)
-        {
-            return await context.Questions.FirstOrDefaultAsync(q => q.QuestionId == id);
-        }
-        public async Task AddQuestionAsync(Question questionToAdd)
-        {
-            await context.Questions.AddAsync(questionToAdd);
-            await context.SaveChangesAsync();
-        }
+		public async Task<List<Question>> GetSegmentQuestionAsync(int z)
+		{
+			var questionSegment = await context.Questions
+				.Include(x => x.Segment)
+				.ThenInclude(c => c.Category).Where(x => x.SegmentId == z)
+				.ToListAsync();
 
-        public async Task UpdateQuestionAsync(Question updatedQuestion)
-        {
-            Question? QuestionUpdate = await GetQuestionByIdAsync(updatedQuestion.QuestionId);
+			return await context.Questions
+				.Include(x => x.Segment)
+				.ThenInclude(c => c.Category).Where(x => x.SegmentId == z)
+				.ToListAsync();
+		}
 
-            if (QuestionUpdate != null)
-            {
-                QuestionUpdate.Questions = updatedQuestion.Questions;
-                QuestionUpdate.Title = updatedQuestion.Title;
-                QuestionUpdate.PossibleAnswers = updatedQuestion.PossibleAnswers;
-                QuestionUpdate.CorrectAnswer = updatedQuestion.CorrectAnswer;
-                QuestionUpdate.Segment = updatedQuestion.Segment;
+		public async Task<Question?> GetQuestionByIdAsync(int id)
+		{
+			return await context.Questions.FirstOrDefaultAsync(q => q.QuestionId == id);
+		}
+		public async Task AddQuestionAsync(Question questionToAdd)
+		{
+			await context.Questions.AddAsync(questionToAdd);
+			await context.SaveChangesAsync();
+		}
 
-                await context.SaveChangesAsync();
-            }
-        }
-        public async Task DeleteQuestionAsync(Question questionToDelete)
-        {
-            try
-            {
-                context.Questions.Remove(questionToDelete);
-                await context.SaveChangesAsync();
-            }
-            catch
-            {
+		public async Task UpdateQuestionAsync(Question updatedQuestion)
+		{
+			Question? QuestionUpdate = await GetQuestionByIdAsync(updatedQuestion.QuestionId);
 
-            }
-        }
-    }
+			if (QuestionUpdate != null)
+			{
+				QuestionUpdate.Questions = updatedQuestion.Questions;
+				QuestionUpdate.Title = updatedQuestion.Title;
+				QuestionUpdate.PossibleAnswers = updatedQuestion.PossibleAnswers;
+				QuestionUpdate.CorrectAnswer = updatedQuestion.CorrectAnswer;
+				QuestionUpdate.Segment = updatedQuestion.Segment;
+
+				await context.SaveChangesAsync();
+			}
+		}
+		public async Task DeleteQuestionAsync(Question questionToDelete)
+		{
+			try
+			{
+				context.Questions.Remove(questionToDelete);
+				await context.SaveChangesAsync();
+			}
+			catch
+			{
+
+			}
+		}
+	}
 
 }
