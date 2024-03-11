6ccdf85677286efc53ef12752017089d68b9181b
JosefMontanari <josef.montanari94@gmail.com>
2024-03-11 15:25:36 +0100

    add help page and inputs

--- ValhallaVaultCyberAwereness/Data/Models/TicketModel.cs
+++ ValhallaVaultCyberAwereness/Data/Models/TicketModel.cs
@@ -6,7 +6,8 @@ namespace ValhallaVaultCyberAwereness.Data.Models
 	{
 		[Key]
 		public int TicketId { get; set; }
-		public List<string> ProblemArea { get; set; } = new List<string>() { "Categories", "Segments", "Questions", "Account", "Login", "Other" };
+		public List<string> ProblemAreas { get; set; } = new List<string>() { "Categories", "Segments", "Questions", "Account", "Login", "Other" };
+		public string ProblemArea { get; set; } = null!;
 		public DateTime TimeSubmitted { get; set; }
 
 		[Required(ErrorMessage = "You must describe your problem in order for us to help you")]
@@ -14,10 +15,7 @@ namespace ValhallaVaultCyberAwereness.Data.Models
 		public string ProblemDescription { get; set; } = null!;
 
 		[Required(ErrorMessage = "Please provide an email in order for us to get back to you")]
-		public EmailAddressAttribute EmailAdress { get; set; } = null!;
+		public string EmailAdress { get; set; } = null!;
 		public ApplicationUser? SubmittedByUser { get; set; }
-		public Category? FaultyCategory { get; set; }
-		public Segment? FaultySegment { get; set; }
-		public Question? FaultyQuestion { get; set; }
 	}
 }
