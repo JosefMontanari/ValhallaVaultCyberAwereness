9e0369867defe054adde61e3b06664250b0732a2
Tim Nordin <74505667+Sidus-96@users.noreply.github.com>
2024-03-12 21:06:53 +0100

    Removed button from welcomepage added authorize on Home pAge
    added comment on API

--- ValhallaVaultCyberAwereness/Controllers/AnswerController.cs
+++ ValhallaVaultCyberAwereness/Controllers/AnswerController.cs
@@ -3,26 +3,27 @@ using ValhallaVaultCyberAwereness.Data;
 
 namespace ValhallaVaultCyberAwereness.Controllers
 {
+	//Låter denna controller vara, lägg på mer funktioner om behov för API skulle finnas. //Tim 
 
-    [Route("api/[controller]")]
-    [ApiController]
-    public class AnswerController : ControllerBase
-    {
-        //spara alla användares svar
+	[Route("api/[controller]")]
+	[ApiController]
+	public class AnswerController : ControllerBase
+	{
+		//spara alla användares svar
 
 
-        private readonly ApplicationDbContext _context;
+		private readonly ApplicationDbContext _context;
 
-        public AnswerController(ApplicationDbContext context)
-        {
-            _context = context;
-        }
+		public AnswerController(ApplicationDbContext context)
+		{
+			_context = context;
+		}
 
-        [HttpPost]
-        public async Task<IActionResult> PostAnswer(List<object> answersFromUser) //kanske ändra object till QuestionModel? men jag tänkte att questionmodel har redan andra properties på sig så kanske lika bra att sända in ett nytt objekt?
-        {
-            //i listan av objekt lägga in alla svar från UI formuläret, typ questionId? svaret. sedan hämta  userID från blazor (googla om vart man hittar id)..
-            return Ok(); //la in return Ok sålänge. 
-        }
-    }
+		[HttpPost]
+		public async Task<IActionResult> PostAnswer(List<object> answersFromUser) //kanske ändra object till QuestionModel? men jag tänkte att questionmodel har redan andra properties på sig så kanske lika bra att sända in ett nytt objekt?
+		{
+			//i listan av objekt lägga in alla svar från UI formuläret, typ questionId? svaret. sedan hämta  userID från blazor (googla om vart man hittar id)..
+			return Ok(); //la in return Ok sålänge. 
+		}
+	}
 }
