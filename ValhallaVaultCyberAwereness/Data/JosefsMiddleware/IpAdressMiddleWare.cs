988e81ad9a0eda36e9d87c5354d4078b4289f8ba
JosefMontanari <josef.montanari94@gmail.com>
2024-03-13 15:05:07 +0100

    add azure and online db and a lot of other stuff

---
+++ ValhallaVaultCyberAwereness/Data/JosefsMiddleware/IpAdressMiddleWare.cs
@@ -0,0 +1,39 @@
+﻿using ValhallaVaultCyberAwereness.FredriksMiddlewareHandler;
+
+namespace ValhallaVaultCyberAwereness.Data.JosefsMiddleware
+{
+    public class IpAdressMiddleWare
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<MWFServices> _logger;
+        private readonly IHttpContextAccessor _contextAccessor;
+
+        public IpAdressMiddleWare(RequestDelegate next, ILogger<MWFServices> logger, IHttpContextAccessor contextAccessor)
+        {
+            _next = next;
+            _logger = logger;
+            _contextAccessor = contextAccessor;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            // Logga msg om man är i någon av dessa sidor
+            if (context.Request.Path.StartsWithSegments("/categorypage") || context.Request.Path.StartsWithSegments("/segmentpage") || context.Request.Path.StartsWithSegments("/subcategory") || context.Request.Path.StartsWithSegments("/questionpage"))
+            {
+                for (var i = 0; i < 100; i++)
+                {
+                    string ip = GetClientIP(_contextAccessor);
+                    _logger.LogInformation("Request received from IP " + ip);
+
+                }
+            }
+            // hanterar nästa request
+            await _next(context);
+        }
+
+        private string GetClientIP(IHttpContextAccessor contextAccessor)
+        {
+            return contextAccessor.HttpContext.Connection.RemoteIpAddress.ToString();
+        }
+    }
+}
---
+++ ValhallaVaultCyberAwereness/Data/KeyManager.cs
@@ -0,0 +1,26 @@
+﻿using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+
+namespace ValhallaVaultCyberAwereness.Data
+{
+    public static class KeyManager
+    {
+        private static string keyVaultName = "vvcs-kv";
+
+        public static async Task<string> GetConnectionString()
+        {
+            var kvUri = "https://" + keyVaultName + ".vault.azure.net";
+
+            var client = new SecretClient(new Uri(kvUri), new DefaultAzureCredential());
+
+            var secret = await client.GetSecretAsync("connectionstring");
+
+            if (secret != null && !string.IsNullOrEmpty(secret.Value.ToString()))
+            {
+                return secret.Value.ToString();
+            }
+
+            throw new Exception("No key found!");
+        }
+    }
+}
